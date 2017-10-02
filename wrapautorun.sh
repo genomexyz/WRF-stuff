b0022b4bf89561783432e84835a6521500fe5a9c
genomexyz <newmr.cyberyan@yahoo.com>
2017-10-02 11:53:17 +0700

    Create wrapautorun.sh

---
+++ wrapautorun.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+./autorunwrf.sh 2016 1 26 31 end
+./autorunwrf.sh 2016 2 1 29 end
+./autorunwrf.sh 2016 3 1 31 end
+./autorunwrf.sh 2016 4 1 30 end
+./autorunwrf.sh 2016 5 1 31 end
+./autorunwrf.sh 2016 6 1 30 end
+./autorunwrf.sh 2016 7 1 31 end
+./autorunwrf.sh 2016 8 1 31 end
+./autorunwrf.sh 2016 9 1 30 end
+./autorunwrf.sh 2016 10 1 31 end
+./autorunwrf.sh 2016 11 1 30 end
+./autorunwrf.sh 2016 12 1 30
