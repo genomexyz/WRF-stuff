2f3f5759f72c4192c67c7cbd7d945b21073a7163
genomexyz <newmr.cyberyan@yahoo.com>
2017-11-23 20:17:03 +0700

    Create wrapujiskema.sh

---
+++ wrapujiskema.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+./ujiskema.sh 1 1 1
+./ujiskema.sh 1 1 0
+
+./ujiskema.sh 1 2 2
+./ujiskema.sh 1 2 0
+
+./ujiskema.sh 1 5 5
+./ujiskema.sh 1 5 0
+
+
+
+./ujiskema.sh 2 1 1
+./ujiskema.sh 2 1 0
+
+./ujiskema.sh 2 2 2
+./ujiskema.sh 2 2 0
+
+./ujiskema.sh 2 5 5
+./ujiskema.sh 2 5 0
+
+
+
+./ujiskema.sh 3 1 1
+./ujiskema.sh 3 1 0
+
+./ujiskema.sh 3 2 2
+./ujiskema.sh 3 2 0
+
+./ujiskema.sh 3 5 5
+./ujiskema.sh 3 5 0
+
+
+
+./ujiskema.sh 5 1 1
+./ujiskema.sh 5 1 0
+
+./ujiskema.sh 5 2 2
+./ujiskema.sh 5 2 0
+
+./ujiskema.sh 5 5 5
+./ujiskema.sh 5 5 0
+
+
+
+./ujiskema.sh 6 1 1
+./ujiskema.sh 6 1 0
+
+./ujiskema.sh 6 2 2
+./ujiskema.sh 6 2 0
+
+./ujiskema.sh 6 5 5
+./ujiskema.sh 6 5 0
