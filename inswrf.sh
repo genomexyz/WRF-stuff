e35f9fd10576e25ba079200bc2346b85729a3329
Muhammad Ryan <nayrhum4code@gmail.com>
2019-10-16 21:35:05 +0700

    Update inswrf.sh

--- inswrf.sh
+++ inswrf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install gcc g++ m4 csh openjdk-7-jre gfortran unzip
+sudo apt-get install gcc g++ m4 csh openjdk-7-jre gfortran unzip make
 
 #setting WRF
 export DIR=~/WRF/LIBRARIES
